9df98ec7926a28d1af96c5fabeb8ecedcf2f61ae
Vorontsov Home PC <oleg-voroncov@mail.ru>
2024-09-20 21:05:54 +0300

    commit from home pc

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Change name");
-        Console.WriteLine("Message added");
+        Console.WriteLine("Home branch");
     }
 }
